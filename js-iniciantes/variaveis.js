6ff80002548269c53d4e376f72205f12065b9d88
Ilanna Freire <ilannafreiret@hotmail.com>
2024-06-22 00:51:44 -0300

    curso 1: variáveis + anotações

---
+++ js-iniciantes/variaveis.js
@@ -0,0 +1,14 @@
+// Variáveis - responsáveis por guardar dados na memória.
+// Inicia com a palavra var, let ou const.
+
+var nome = 'Ilanna';
+let idade = 28;
+const possuiFaculdade = true;
+
+// Evite repetições 
+// DRY(Don't repeat yourself)
+
+var preco = 20;
+var totalComprado = 5;
+var precoTotal = preco * totalComprado;
+
