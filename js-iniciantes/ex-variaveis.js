aec04ec3076d59002b7965a3e927f3a069f084a3
Ilanna Freire <ilannafreiret@hotmail.com>
2024-06-22 20:12:20 -0300

    C1: exercícios variáveis js

---
+++ js-iniciantes/ex-variaveis.js
@@ -0,0 +1,26 @@
+
+
+// Declarar uma variável com o seu nome
+
+var nome = 'Ilanna'
+
+// Declarar uma variável com a sua idade
+
+var idade = 28;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+
+comidaFavorita = 'sushi';
+
+// Declarar 5 variáveis diferentes sem valores
+
+var var1;
+var var2;
+var var3;
+var var4;
+var var5;
