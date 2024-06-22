25402a499501f7ad7740c0e8327a1cf8a695ca67
Ilanna Freire <ilannafreiret@hotmail.com>
2024-06-22 20:30:01 -0300

    C1: 7 tipos de dados

---
+++ js-iniciantes/dados.js
@@ -0,0 +1,14 @@
+// 7 Tipos de dados
+// Todos são primitivos exceto os objetos.
+
+var nome = 'Ilanna'; // String
+var idade = 28; // Number
+var possuiFaculdade = true; // Boolean
+var time; // Undefined
+var comidade = null; // Null
+var simbolo = Symbol() // Symbol
+var novoObjeto = {} // Object 
+
+// Primitivos são dados imutáveis. 
+
+
