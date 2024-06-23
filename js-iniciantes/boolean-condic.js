1a4c9c044e9d93534869156e46a570f0278a2b77
Ilanna Freire <ilannafreiret@hotmail.com>
2024-06-23 03:38:53 -0300

    C1: condicionais IF e ELSE

--- js-iniciantes/boolean-condic.js
+++ js-iniciantes/boolean-condic.js
@@ -5,3 +5,21 @@
 
 var possuiGraduacao = true;
 var possuiDoutorado = false;
+
+// Condicionais IF e ELSE 
+
+// Verificar se uma expressão é verdadeira com if, caso contrário
+// o else será ativido. 
+
+var possuiGraduacao = true; 
+
+if(possuiGraduacao) {
+    console.log('Possui graduação');
+} else {
+    console.log('Não possui graduação');
+}
+
+// retorna Possui graduação e não executa o else. 
+
+// O valor dentro dos parênteses sempre será avaliado em false ou true.
+
