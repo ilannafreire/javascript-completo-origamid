9d5cbea8606957f4a51288b3425d633e21411870
Ilanna Freire <ilannafreiret@hotmail.com>
2024-06-22 21:35:56 -0300

    C1: exercicios strings

---
+++ js-iniciantes/ex-string.js
@@ -0,0 +1,26 @@
+// Declare uma variável contendo uma string
+
+let message = "Especialista Java e Angular em Cloud"
+
+// Declare uma variável contendo um número dentro de uma string
+
+let numberInString = "This number is 51";
+
+// Declare uma variável com a sua idade
+
+var myAge = 28;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+let firstName = "Ilanna";
+let lastName = "Freire";
+let fullName = firstName + " " + lastName;
+
+// Coloque a seguinte frase em uma variável: It's time
+
+let timePhrase = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof firstName); 
+
