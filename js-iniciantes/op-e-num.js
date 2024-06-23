97e3c50fa2cf8545f7c260e8bee69e4f4a626aa5
Ilanna Freire <ilannafreiret@hotmail.com>
2024-06-22 22:52:11 -0300

    C1: operadores e números

---
+++ js-iniciantes/op-e-num.js
@@ -0,0 +1,29 @@
+// Números 
+
+var idade = 28;
+var gols = 1000;
+var pi = 3.14; // ponto para decimal 
+var exp = 2e10; // 20000000000
+
+// Precisão para até 15 dígitos
+
+// Operadores aritméticos 
+
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50
+var multiplicacao = 100 * 2; // 200
+var divisao = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5; // 4
+
+// Lembrando que soma + em Strings serve para concatenar.
+
+// Operadores aritméticos (Strings)
+
+var soma = '100' + 50; // 10050
+var subtracao = '100' - 50; // 50
+var multiplicacao = '100' * '2'; // 200
+var divisao = 'Comprei 10' / 2; // NaN (Not a Number)
+
+// É possível verificar se uma variável é NaN ou 
+// não com a função isNaN()
