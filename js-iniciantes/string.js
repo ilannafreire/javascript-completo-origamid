4b754202d033261e4828dc849b10f42be71c10c9
Ilanna Freire <ilannafreiret@hotmail.com>
2024-06-22 21:15:07 -0300

    C1: string + anotações

---
+++ js-iniciantes/string.js
@@ -0,0 +1,12 @@
+// Template string 
+
+var gols = 1000;
+var frase1 = 'Romário fez ' + gols + ' gols';
+var frase2 = `´Romário fez  ${gols} gols`; //Utilizando Template String
+
+// você deve passar expressões / variáveis dentro do ${gols}
+
+
+
+
+
