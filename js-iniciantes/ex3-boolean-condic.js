89008108ab0686addcff647b07a791ff494a8c16
Ilanna Freire <ilannafreiret@hotmail.com>
2024-06-27 01:30:02 -0300

    C1: exercício - o que aparecerá no console?

---
+++ js-iniciantes/ex3-boolean-condic.js
@@ -0,0 +1,14 @@
+// O que irá aparecer no console?
+
+
+if(('Gato' === 'gato') && (5 > 2)) {
+    console.log('Verdadeiro');
+  } else {
+    console.log('Falso');
+  }
+
+
+  /* 'Gato' === 'gato': Esta comparação verifica se a string 'Gato' é exatamente igual à string 'gato'. 
+  Como a comparação é sensível a maiúsculas e minúsculas, 'Gato' não é igual a 'gato', então esta expressão é false.
+  5 > 2: Esta comparação verifica se 5 é maior que 2, o que é verdade, então esta expressão é true.
+  */
