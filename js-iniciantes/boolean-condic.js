267f8ce9c33a182185a901b565e3315094721fe2
Ilanna Freire <ilannafreiret@hotmail.com>
2024-06-27 00:12:46 -0300

    C1 : mais operadores de comparação

--- js-iniciantes/boolean-condic.js
+++ js-iniciantes/boolean-condic.js
@@ -117,3 +117,15 @@ if(true)
     20 < 10; // false
     10 <= 10 // true
     10 >= 11 // false 
+
+// Mais operadores de comparação
+
+/* O == faz uma comparação não tão estrita e o === faz uma comparação estrita, ou seja 
+o tipo de dado deve ser o mesmo quando usamos.
+*/
+    10 == '10'; //true
+    10 == 10; // true
+    10 === '10'; // false 
+    10 === 10 // true 
+    10 != 15 // true
+    10 !== '10' // true 
