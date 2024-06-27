d7086b1c07cc7d8a87e9fdd47aff1a2fb8256cc3
Ilanna Freire <ilannafreiret@hotmail.com>
2024-06-27 02:00:24 -0300

    C1: funções - parâmetros e argumentos

--- js-iniciantes/funcoes.js
+++ js-iniciantes/funcoes.js
@@ -12,3 +12,23 @@ areaQuadrado(5) // 25
 areaQuadrado(2) // 4
 
 // Chamada de function declaration
+// Funções - parênteses () executam uma função.
+
+// Parâmetros e argumentos
+
+/* Ao criar uma função, você pode definir parâmetros
+   Ao executar uma função, você pode passar argumentos.
+*/
+
+// peso e altura são parâmetros
+ function imc(peso, altura) {
+    const imc  = peso / (altura ** 2);
+    return imc;
+ }
+
+ imc(80, 1.80) // 80 e 1.80 são os argumentos
+ imc(60, 1.70) // 60 e 1.70 são argumentos
+
+ /* Separar por uma vírgula cada parâmetro. Você pode redefinir mais de um
+ parâmetro ou nenhum também.
+ */
