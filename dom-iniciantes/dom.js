1a5a501b035f5f3e04b7e4a57731443bc8d21206
Ilanna Freire <ilannafreiret@hotmail.com>
2024-08-07 05:21:29 -0300

    C2: DOM

---
+++ dom-iniciantes/dom.js
@@ -0,0 +1,12 @@
+// Document Object Model (DOM)
+
+/* Interface que representa HTML e XML por meio 
+de objetos. É possivel, com essa interface, manipular a estrutura, estilo 
+e conteúdo destes documentos.
+*/
+
+console.log(window);
+// window é um objeto global do browser
+// possui diferentes métodos e propriedades
+
+window.innerHeight; // retorna a altura do browser
