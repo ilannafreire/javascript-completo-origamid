2698549f0b3ba30d857db49f660da9ae0b63236a
Ilanna Freire <ilannafreiret@hotmail.com>
2024-08-07 04:42:43 -0300

    C1: tudo é objeto em js.

---
+++ js-iniciantes/tudo-objeto.js
@@ -0,0 +1,15 @@
+// Tudo é objeto
+
+/* Strings, Números, Boolean, Objetos e mais, possuem
+propriedades e métodos e por isso sáo objetos.
+*/
+
+var nome = 'Ilanna';
+
+nome.length; //5
+nome.charAt(1); // 'n'
+nome.replace('ré', 'rei'); // 'Ilanna'
+nome; // 'Ilanna'
+
+// Uma string herda propriedades e métodos do construtor String()
+
