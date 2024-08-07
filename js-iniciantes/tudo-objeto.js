efd54e3d70b8d8d931dc7ad7303c1cd01243c38c
Ilanna Freire <ilannafreiret@hotmail.com>
2024-08-07 05:04:39 -0300

    C1: tudo objeto em js

--- js-iniciantes/tudo-objeto.js
+++ js-iniciantes/tudo-objeto.js
@@ -32,3 +32,15 @@ areaQuadrado.toString();
 //}"
 
 areaQuadrado.length; // 1
+
+// Elementos do DOM
+
+<a class= "btn"> Clique</a>
+
+var btn = document.querySelector('.btn');
+
+btn.classList.add('azul') // adiciona a classe azul
+btn.innerText; // 'Clique'
+btn.addEventListener('click', function(){
+    console.log('Clicou')
+}) 
