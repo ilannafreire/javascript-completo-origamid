b294a4736ef032fcbcb0345460382d551edba6a8
Ilanna Freire <ilannafreiret@hotmail.com>
2024-08-17 11:25:08 -0300

    C2: window and document

---
+++ dom-iniciantes/window-document.js
@@ -0,0 +1,14 @@
+// Window e document
+
+/* São objetos principais do DOM, boa parte da manipulação é feita
+por meio dos métodos e propriedades.
+
+window - é objeto global, por isso não precisamos chamar ele na frente dos 
+seus métodos e propriedades. 
+*/
+
+window.alert('Isso é um alerta');
+alert('Isso é um alerta'); 
+
+document.querySelector('h1'); // Seleciona o primeiro h1
+document.body; // Retorna o body
