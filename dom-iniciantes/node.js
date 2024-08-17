83888dcf6669800c27ff49d40bea7a675c2c3516
Ilanna Freire <ilannafreiret@hotmail.com>
2024-08-17 11:29:50 -0300

    C2: node

---
+++ dom-iniciantes/node.js
@@ -0,0 +1,17 @@
+// Node
+
+/* Toda tag html é representada pelo objeto Element e por isso
+herda os seus métodos e propriedades. 
+Element é um tipo de objeto Node.
+*/
+
+
+const titulo = document.querySelector('h1');
+
+titulo.innerText; // retorna o texto
+titulo.classList; // retorna as classes
+titulo.id;  // retorna o id
+titulo.offsetHeight; // retorna a altura do elemento
+
+titulo.addEventListener('click', callback,);
+// ativa uma função callback ao click no titulo
